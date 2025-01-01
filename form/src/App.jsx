16cb918f5557090d7d4262be3c9d8e25c2ccdc8c
sankalp patel <sankalppatel38@gmail.com>
2025-01-01 17:18:01 +0530

    code_optimise

--- form/src/App.jsx
+++ form/src/App.jsx
@@ -1,141 +1,22 @@
 import { useState } from "react";
 import "./App.css";
-
-// single checkbox
-const InitialState = () => ({
-  name: "",
-  course: "",
-  unit: "",
-  batch: "",
-  result: false,
-  result2: false,
-  country: "",
-});
-
-// Multiple selection checkbox
-// const InitialState = () => ({
-//   name: "",
-//   course: "",
-//   unit: "",
-//   batch: "",
-//   result: [] or {},
-//   country: "",
-// });
+import DashForm from './Components/DashForms';
 
 function App() {
-  const [isInput, setisInput] = useState(InitialState);
+  const [data, setData] = useState([]);
 
-  // Single selection checkbox handling done by assigning different states for different checkbox
-  const HandleInputChange = (e) => {
-    const { name, value, type, checked } = e.target;
-  
-    const newValue = type === "checkbox" ? checked : value;
-    
-    setisInput((oldState) => {return{
-        ...oldState,
-        [name] : newValue
-    }})
+
+
+  const HandleInputChange= (InputData) => {
+    setData(oldState=>[...oldState,InputData])
   };
 
-// Multiple selection checkbox handling done by assigning array or object
-  // const HandleInputChange = (e) => {
-  //   const { name, value, type, checked } = e.target;
-  
-  //     if (type === 'checkbox') {
-  //       setisInput(oldState => ({
-  //         ...oldState,
-  //         [name]: checked ? [...oldState[name],value] : oldState[name].filter((item)=>item !==value),
-  //       }));
-  //     }
-  //   else {
-  //     setisInput(oldState => ({
-  //       ...oldState,
-  //       [name]: value,
-  //     }));
-  //   }
-  // };
 
-  const HandleSubmit = (e) => {
-    e.preventDefault();
-    console.log(isInput);
-    setisInput(InitialState);
-  };
+  console.log(data);
 
   return (
     <>
-      <div>
-        <h2>Student Dashboard</h2>
-        <form id="form" onSubmit={HandleSubmit}>
-          <input
-            type="text"
-            value={isInput.name}
-            name="name"
-            placeholder="Enter Name"
-            onChange={HandleInputChange}
-          />
-          <br />
-          <br />
-          <input
-            type="text"
-            value={isInput.course}
-            name="course"
-            placeholder="Enter course"
-            onChange={HandleInputChange}
-          />
-          <br />
-          <br />
-          <input
-            type="text"
-            value={isInput.unit}
-            name="unit"
-            placeholder="Enter Unit"
-            onChange={HandleInputChange}
-          />
-          <br />
-          <br />
-          <input
-            type="text"
-            value={isInput.batch}
-            name="batch"
-            placeholder="Enter Batch"
-            onChange={HandleInputChange}
-          />
-          <br />
-          <br />
-          <label htmlFor="result">
-            Pass
-            <input
-              type="checkbox"
-              name="result"
-              value="pass"
-              checked={isInput.result}
-              onChange={HandleInputChange}
-            />
-          </label>
-          <label htmlFor="result">
-            Fail
-            <input
-              type="checkbox"
-              name="result2"
-              value="fail"
-              checked={isInput.result2}
-              onChange={HandleInputChange}
-            />
-          </label>
-          <br />
-          <br />
-          <select name="country" id="country" onChange={HandleInputChange}>
-            <option value="">select country</option>
-            <option value="in">India</option>
-            <option value="usa">USA</option>
-            <option value="ireland">Ireland</option>
-            <option value="france">France</option>
-          </select>
-          <br />
-          <br />
-          <input type="submit" value="submit" />
-        </form>
-      </div>
+     <DashForm CallBack={HandleInputChange} />
     </>
   );
 }
---
+++ form/src/Components/DashForms.jsx
@@ -0,0 +1,113 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+const InitialState = () => ({
+  name: "",
+  course: "",
+  unit: "",
+  batch: "",
+  pass: false,
+  fail: false,
+  country: "",
+});
+const DashForm = ({CallBack}) =>{
+  
+  const [isInput, setIsInput] = useState(InitialState);
+
+
+  const HandleInputChange = (e) => {
+    const { name, value, type, checked } = e.target;
+  
+    const newValue = type === "checkbox" ? checked : value;
+    
+    setIsInput((oldState) => {return{
+        ...oldState,
+        [name] : newValue
+    }})
+  };
+  
+  const HandleSubmit = (e) => {
+    e. preventDefault();
+    CallBack(isInput);
+    setIsInput(InitialState);
+  }
+
+    return(
+        <>
+         <div>
+        <h2>Student Dashboard</h2>
+        <form id="form" onSubmit={HandleSubmit}>
+          <input
+            type="text"
+            value={isInput.name}
+            name="name"
+            placeholder="Enter Name"
+            onChange={HandleInputChange}
+          />
+          <br />
+          <br />
+          <input
+            type="text"
+          
+            value={isInput.course}
+            name="course"
+            placeholder="Enter course"
+            onChange={HandleInputChange}
+          />
+          <br />
+          <br />
+          <input
+            type="text"
+            value={isInput.unit}
+            name="unit"
+            placeholder="Enter Unit"
+            onChange={HandleInputChange}
+          />
+          <br />
+          <br />
+          <input
+            type="text"
+            value={isInput.batch}
+            name="batch"
+            placeholder="Enter Batch"
+            onChange={HandleInputChange}
+          />
+          <br />
+          <br />
+          <label htmlFor="result">
+            Pass
+            <input
+              type="checkbox"
+              name="pass"
+              checked={isInput.pass}
+              onChange={HandleInputChange}
+            />
+          </label>
+          <label htmlFor="result">
+            Fail
+            <input
+              type="checkbox"
+              name="fail"
+              checked={isInput.fail}
+              onChange={HandleInputChange}
+            />
+          </label>
+          <br />
+          <br />
+          <select name="country" id="country" onChange={HandleInputChange}>
+            <option value="">select country</option>
+            <option value="in">India</option>
+            <option value="usa">USA</option>
+            <option value="ireland">Ireland</option>
+            <option value="france">France</option>
+          </select>
+          <br />
+          <br />
+          <input type="submit" value="submit" />
+        </form>
+      </div>
+        </>
+    )
+}
+
+export default DashForm;
