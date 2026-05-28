8aa0fa6f57bd28d7c0e0397b567ad95f6b861b60
sankalp patel <sankalppatel38@gmail.com>
2026-05-28 18:57:49 +0530

    Implement product listing feature with SWR for data fetching
    - Added DataContextProvider for global data state management.
    - Created ProductCard and ProductPage components to display products.
    - Integrated SWR for fetching product data from an API in the Dashboard component.
    - Enhanced App.css with styles for product layout and card design.
    - Updated main.jsx to include DataContextProvider for managing product data.

--- mini-app2/src/Pages/Login.jsx
+++ mini-app2/src/Pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState, useContext } from 'react'
 import axios from 'axios'
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { GlobalAuth } from '../Context/AuthContext'
 import { Action_Type } from '../Helpers/Action_Creators'
 
@@ -10,12 +10,10 @@ const InitialState = {
 }
 
 function Login() {
-  
   const [isInput, setIsInput] = useState(InitialState)
-  const {state, dispatch} = useContext(GlobalAuth);
-  console.log(state);
-  const nav = useNavigate();
-  
+  const { state, dispatch } = useContext(GlobalAuth)
+  const nav = useNavigate()
+
   const HandleChange = (e) => {
     const { name, value } = e.target
     setIsInput((prevState) => ({
@@ -23,56 +21,53 @@ function Login() {
       [name]: value,
     }))
   }
-  
 
-
-  const HandleSubmit = async(event) => {
+  const HandleSubmit = async (event) => {
     event.preventDefault()
 
-  if(!isInput.username || !isInput.password){
-      alert("FIELDS ARE REQUIRED TO LOGIN")
+    if (!isInput.username || !isInput.password) {
+      alert('FIELDS ARE REQUIRED TO LOGIN')
       return
     }
 
     try {
       dispatch({
-        type: Action_Type.Login_Request
+        type: Action_Type.Login_Request,
       })
-      let url = 'https://dummyjson.com/auth/login';
-      let res = await axios.post(url,isInput);
-      if(!res.status) throw new Error("Something is Wrong");
-      const token = res?.data?.accessToken || null;
-      if(token){
+      let url = 'https://dummyjson.com/auth/login'
+      let res = await axios.post(url, isInput)
+      if (!res.status) throw new Error('Something is Wrong')
+      const token = res?.data?.accessToken || null
+      if (token) {
         dispatch({
           type: Action_Type.Login_Success,
-          payload:{
-            Token: token || null
-          }
+          payload: {
+            Token: token || null,
+          },
         })
-        nav('/dashboard');
-      }else{
+        nav('/dashboard')
+      } else {
         dispatch({
           type: Action_Type.Login_Failure,
-          payload:{
-            error: "Token is Missing"
-          }
+          payload: {
+            error: 'Token is Missing',
+          },
         })
       }
-      
     } catch (error) {
       dispatch({
         type: Action_Type.Login_Failure,
-        payload:{
-          error: error.message
-        }
+        payload: {
+          error: error.message,
+        },
       })
     }
   }
 
   return (
     <div className="Form">
-      {state.isLoading && (<h2>Loading</h2>)}
-      {state.isError && (<h2>{state.isError}</h2>)}
+      {state.isLoading && <h2>Loading</h2>}
+      {state.isError && <h2>{state.isError}</h2>}
       <form onSubmit={HandleSubmit}>
         <input
           type="text"
