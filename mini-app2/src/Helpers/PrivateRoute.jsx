a214f5f8859fe296fe3d733de71e4e5b2f3f9164
sankalp patel <sankalppatel38@gmail.com>
2026-05-28 22:37:05 +0530

    fix_privateRoute

--- mini-app2/src/Helpers/PrivateRoute.jsx
+++ mini-app2/src/Helpers/PrivateRoute.jsx
@@ -4,7 +4,7 @@ import { Navigate } from 'react-router-dom'
 function PrivateRoute({ children }) {
   const { state } = useContext(GlobalAuth)
 
-  if (state.isAuth && state.token)
+  if (!state.isAuth && !state.token)
     return <Navigate to="/login" replace={true} />
   return children
 }
